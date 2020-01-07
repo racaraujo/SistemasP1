9cbb658a9027afcf33ec44a1566dc5acf62afd2b
IgorGarciaCosta <igonildo7@gmail.com>
2020-01-07 09:19:03 -0300

    Update initialize_lcd.v

--- src/quartus_project/verilog/custom_instructions/initialize_lcd.v
+++ src/quartus_project/verilog/custom_instructions/initialize_lcd.v
@@ -1,4 +1,4 @@
-`timescale 1ms/1000 us
+
 module LCD_Initializer (
 	dataa,
 	datab,
@@ -28,8 +28,121 @@ output reg lcd_rw;
 output reg [7:0] lcd_data;
 output reg lcd_enable;
 
-reg [1:0] executed;
-reg [31:0] temp_result;
+reg [2:0] state;
+
+always @ (posedge clk or posedge reset) begin
+
+	if (reset) begin
+		counter <= 16'd0;
+		state   <= 3'b0;
+		lcd_rs  <= 1'b0;
+		lcd_rw  <= 1'b0;
+		lcd_enable  <= 1'b0;
+		lcd_data  <= 8'b0;
+	end else begin
+		if (clk_en) begin
+				case (state)
+					3'b001:begin //initializing stage 1, Return Home: Set DDRAM address to 8'h00
+						done <= 1'b0;
+						lcd_enable <= 1'b1;
+						if (start) begin
+							state <= 3'b010;
+							lcd_rs <= 1'b0;
+							lcd_rw <= 1'b0;
+							lcd_data <= 8'h02;
+							counter <= 16'd0;
+						end else begin
+							state <= 2'b01;
+						end
+					end
+					3'b010:begin //initializing stage 2, Send Function Set
+						done <= 1'b0;
+						lcd_enable <= 1'b1;
+						if (counter == 16'd50_000) begin
+							state <= 3'b011;
+							lcd_data <= 8'h38;
+							counter <= 16'd0;
+						end else begin
+							counter <= counter + 1'd1;
+							state <= 3'b010;
+						end
+						
+					3'b011:begin //initializing stage 3, Send Display On/Off Control
+						done <= 1'b0;
+						lcd_enable <= 1'b1;
+						if (counter == 16'd50_000) begin
+							state <= 3'b100;
+							lcd_data <= 8'h0C;
+							counter <= 16'd0;
+						end else begin
+							counter <= counter + 1'd1;
+							state <= 3'b011;
+						end	
+						
+					3'b100:begin //initializing stage 4, Send entry mode set
+						done <= 1'b0;
+						lcd_enable <= 1'b1;
+						if (counter == 16'd50_000) begin
+							state <= 3'b101;
+							lcd_data <= 8'h06;
+							counter <= 16'd0;
+						end else begin
+							counter <= counter + 1'd1;
+							state <= 3'b100;
+						end
+						
+					3'b101:begin //initializing stage 5, Send display clear
+						done <= 1'b0;
+						lcd_enable <= 1'b1;
+						if (counter == 16'd50_000) begin
+							state <= 3'b110;
+							lcd_data <= 8'h01;
+							counter <= 16'd0;
+						end else begin
+							counter <= counter + 1'd1;
+							state <= 3'b101;
+						end
+						
+					3'b110:begin //initializing next stage. deve enviar a imagem pro lcd
+						done <= 1'b1;
+						lcd_enable <= 1'b1;
+						if (counter == 16'd50_000) begin
+							state <= ;
+							counter <= 16'd0;
+						end else begin
+							counter <= counter + 1'd1;
+							state <= 3'b110;
+						end	
+						
+					
+					2'b10:begin // espera 1ms com en = 1; working
+						done <= 1'b0;
+						if (counter == 16'd50_000) begin
+							state <= 2'b11;
+							en    <= 1'b0;
+							counter <= 16'd0;
+						end else begin
+							contador <= contador + 1'd1;
+							state <= 2'b10;
+						end
+					end
+					2'b11:begin //espera 1ms com en = 0; finish
+						if (contador == 16'd50_000) begin
+							done <= 1'b1;
+							result <= 1'b1;
+							state <= 2'b01;
+							contador <= 16'd0;
+							en <= 1'b0;
+						end else begin
+							contador <= contador + 16'd1;
+						end
+					end
+				endcase
+			end
+	
+	end
+end
+
 initial begin
 	lcd_rs <= 1'b0;
 	lcd_rw <= 1'b0;
